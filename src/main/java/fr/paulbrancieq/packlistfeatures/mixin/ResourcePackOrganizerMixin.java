b23d5e415c12133c24c65292e367f3a1b86194f8
Paul Brancieq <paul.brancieq@epitech.eu>
2024-01-17 12:54:23 +0100

    fix: Fabric resource pack position after game start
    Odd thing, Fabric Mod Resources was always at the top of the list even if loaded at the good position. Corrected by always checking order on ResourcePackOrganizer creation.

---
+++ src/main/java/fr/paulbrancieq/packlistfeatures/mixin/ResourcePackOrganizerMixin.java
@@ -0,0 +1,34 @@
+package fr.paulbrancieq.packlistfeatures.mixin;
+
+import fr.paulbrancieq.packlistfeatures.PackListFeaturesMod;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.pack.ResourcePackOrganizer;
+import net.minecraft.resource.ResourcePackManager;
+import net.minecraft.resource.ResourcePackProfile;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.*;
+
+@Mixin(ResourcePackOrganizer.class)
+public abstract class ResourcePackOrganizerMixin {
+    @Shadow @Final
+    List<ResourcePackProfile> enabledPacks;
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    private void constructor(CallbackInfo ci) {
+        Map<String, ResourcePackProfile> map = new HashMap<>();
+        List<String> nameList = new ArrayList<>();
+        enabledPacks.forEach(pack -> map.put(pack.getDescription().getString(), pack));
+        enabledPacks.forEach(pack -> nameList.add(pack.getDescription().getString()));
+        Collections.reverse(nameList);
+        List<ResourcePackProfile> temp = PackListFeaturesMod.getInstance().getPackIndexManager().organizePacks(map, nameList);
+        Collections.reverse(temp);
+        enabledPacks.clear();
+        enabledPacks.addAll(temp);
+    }
+}
